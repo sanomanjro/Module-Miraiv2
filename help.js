e53bcd555c9976436bc2efcbb37216c2f2d714a8
DuyVuong <86643647+ProDuyVuong@users.noreply.github.com>
2021-07-15 17:20:24 +0700

    Update help.js

--- help.js
+++ help.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, args, Users, event, Threads, utils, client }) {
-var msg = ["hiện tại code này là do DuyVuong code ở phiên bản 1.0.2","on bot này thông minh hơn bạn"//tự thêm nhé];
+var msg = ["hiện tại code này là do DuyVuong code ở phiên bản 1.0.2","con bot này thông minh hơn bạn"//tự thêm nhé];
 var cc = Math.floor(Math.random() * msg.length);
 var lon = "[Bạn có biết?]: ${msg}.";
 return api.sendMessage($lon, event.threadID, event.messageID);
